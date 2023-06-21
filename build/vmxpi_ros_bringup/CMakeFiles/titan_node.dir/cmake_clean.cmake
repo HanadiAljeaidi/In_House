c7f9681db4cf32d7ee995face2fd76cf3a0f0228
root <Hanadi.Aljeaidi77@gmail.com>
2023-06-21 04:27:39 -1000

    the second commit

---
+++ build/vmxpi_ros_bringup/CMakeFiles/titan_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/pi/catkin_ws/devel/.private/vmxpi_ros_bringup/lib/vmxpi_ros_bringup/titan_node"
+  "/home/pi/catkin_ws/devel/.private/vmxpi_ros_bringup/lib/vmxpi_ros_bringup/titan_node.pdb"
+  "CMakeFiles/titan_node.dir/src/titan_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/titan_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/vmxpi_ros_bringup/CMakeFiles/ultrasonic_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/pi/catkin_ws/devel/.private/vmxpi_ros_bringup/lib/vmxpi_ros_bringup/ultrasonic_node"
+  "/home/pi/catkin_ws/devel/.private/vmxpi_ros_bringup/lib/vmxpi_ros_bringup/ultrasonic_node.pdb"
+  "CMakeFiles/ultrasonic_node.dir/src/ultrasonic_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ultrasonic_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
